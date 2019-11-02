c39feaee89e43cd12900b5168dff6bc3f5ca1e29
Piekarski Maciej <44343@etu.he2b.be>
2019-11-02 19:19:52 +0100

    Generic function to handle CSV insertion

--- app/Http/Controllers/ProfesseurController.php
+++ app/Http/Controllers/ProfesseurController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Professeur;
-use Exception;
+use App\Utilitaries\Util;
 use Illuminate\Http\Request;
-use Session;
 
 class ProfesseurController extends Controller
 {
@@ -89,74 +88,9 @@ class ProfesseurController extends Controller
 
     public function uploadFile(Request $request)
     {
-        if ($request->file == null) {
-            Session::flash('warning', 'Please choose a file');
-        } else {
-            $file = $request->file('file');
-
-            // File details
-            $filename = $file->getClientOriginalName();
-            $extension = $file->getClientOriginalExtension();
-            $tempPath = $file->getRealPath();
-            $fileSize = $file->getSize();
-            $mimeType = $file->getMimeType();
-
-            // File extension
-            $valid_extension = array("csv");
-
-            // 2MB in bytes
-            $maxFileSize = 2097152;
-
-            // Check file extension
-            if (in_array(strtolower($extension), $valid_extension)) {
-
-                // Check file size
-                if ($fileSize <= $maxFileSize) {
-                    try {
-                        $location = 'uploads';
-                        $file->move($location, $filename);
-                        $filepath = public_path($location . "/" . $filename);
-                        $file = fopen($filepath, "r");
-                        $importData_arr = array();
-                        $i = 0;
-
-                        while (($filedata = fgetcsv($file, 1000, ",")) !== false) {
-                            $num = count($filedata);
-
-                            if ($i == 0) {
-                                $i++;
-                                continue;
-                            }
-                            for ($c = 0; $c < $num; $c++) {
-                                $importData_arr[$i][] = $filedata[$c];
-                            }
-                            $i++;
-                        }
-
-                        fclose($file);
-
-                        foreach ($importData_arr as $importData) {
-                            $insertData = array(
-                                "acronyme" => $importData[0],
-                                "nom" => $importData[1],
-                                "prenom" => $importData[2]);
-
-                            Professeur::insertData($insertData);
-                        }
-
-                        Session::flash('success', 'Import Successful');
-                    } catch (Exception $e) {
-                        // return back()->withError($e->getMessage())->withInput();
-                        // Session::flash('warning', $e->getMessage());
-                        Session::flash('warning', 'The content of the file is inappropriate and cannot be processed. Check if it\'s well formated and the data is correct');
-                    }
-                } else {
-                    Session::flash('warning', 'File too large. File must be less than 2MB');
-                }
-            } else {
-                Session::flash('warning', 'Invalid File Extension');
-            }
-        }
+        Util::handleCSVInsertion($request, [
+            "acronyme", "nom", "prenom",
+        ], Professeur::class);
 
         return redirect('professeurs');
     }
---
+++ app/Utilitaries/Util.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Utilitaries;
+
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Session;
+
+class Util
+{
+
+    /**
+     * Handle the insertion from a CSV file.
+     *
+     * @param array $attributes Attributes of the model.
+     * @param model $model Model to handle.
+     */
+    public static function handleCSVInsertion($request, $attributes, $model)
+    {
+        if ($request->file == null) {
+            Session::flash('warning', 'Please choose a file');
+        } else {
+            $file = $request->file('file');
+
+            // File details
+            $filename = $file->getClientOriginalName();
+            $extension = $file->getClientOriginalExtension();
+            $tempPath = $file->getRealPath();
+            $fileSize = $file->getSize();
+            $mimeType = $file->getMimeType();
+
+            // File extension
+            $valid_extension = array("csv");
+
+            // 2MB in bytes
+            $maxFileSize = 2097152;
+
+            // Check file extension
+            if (in_array(strtolower($extension), $valid_extension)) {
+
+                // Check file size
+                if ($fileSize <= $maxFileSize) {
+                    try {
+                        $location = 'uploads';
+                        $file->move($location, $filename);
+                        $filepath = public_path($location . "/" . $filename);
+                        $file = fopen($filepath, "r");
+                        $importData_arr = array();
+                        $i = 0;
+
+                        while (($filedata = fgetcsv($file, 1000, ",")) !== false) {
+                            $num = count($filedata);
+
+                            if ($i == 0) {
+                                $i++;
+                                continue;
+                            }
+                            for ($c = 0; $c < $num; $c++) {
+                                $importData_arr[$i][] = $filedata[$c];
+                            }
+                            $i++;
+                        }
+
+                        fclose($file);
+
+                        foreach ($importData_arr as $importData) {
+                            $insertData = array();
+
+                            for ($index = 0; $index < count($attributes); $index++) {
+                                $insertData[$attributes[$index]] = $importData[$index];
+                            }
+
+                            $model::insertData($insertData);
+                        }
+
+                        Session::flash('success', 'Import Successful');
+                    } catch (Exception $e) {
+                        // return back()->withError($e->getMessage())->withInput();
+                        // Session::flash('warning', $e->getMessage());
+                        Session::flash('warning', 'The content of the file is inappropriate and cannot be processed. Check if it\'s well formated and the data is correct');
+                    }
+                } else {
+                    Session::flash('warning', 'File too large. File must be less than 2MB');
+                }
+            } else {
+                Session::flash('warning', 'Invalid File Extension');
+            }
+        }
+    }
+
+}
