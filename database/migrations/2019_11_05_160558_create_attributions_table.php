61b020a3f4f3c891a360fe701032f3f26a94adc5
Logan Farci <47923@etu.he2b.be>
2019-11-05 17:58:58 +0100

    attributions_list: AttributionTest created

--- database/migrations/2019_11_05_160558_create_attributions_table.php
+++ database/migrations/2019_11_05_160558_create_attributions_table.php
@@ -14,10 +14,17 @@ class CreateAttributionsTable extends Migration
     public function up()
     {
         Schema::create('attributions', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('professor_acronyme', 3);
+            $table->string('course_id', 5);
+            $table->string('group_id', 5);
+            $table->integer('quadrimester'); // TODO: check between 1 and 6
+
             $table->foreign('professor_acronyme')->references('acronyme')->on('professeurs');
             $table->foreign('course_id')->references('id')->on('courses');
             $table->foreign('group_id')->references('nom')->on('groupes');
-            $table->integer('quadrimester');
+
+            $table->unique(['course_id', 'group_id', 'quadrimester']);
         });
     }
 
