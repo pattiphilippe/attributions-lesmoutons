0c97d20c4e2386a7ba1cb678a6c573baa786338d
Logan Farci <47923@etu.he2b.be>
2019-11-05 17:57:03 +0100

    attributions_list: model and migrations added

---
+++ database/migrations/2019_11_05_160558_create_attributions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributions', function (Blueprint $table) {
+            $table->foreign('professor_acronyme')->references('acronyme')->on('professeurs');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('group_id')->references('nom')->on('groupes');
+            $table->integer('quadrimester');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributions');
+    }
+}
