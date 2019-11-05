34c7eb38fc4e8b457fc71db48ff370a3d3ad2220
Logan Farci <47923@etu.he2b.be>
2019-11-05 16:19:04 +0100

    Course Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(ProfesseursTableSeeder::class);
         $this->call(GroupesTableSeeder::class);
+        $this->call(CoursesTableSeeder::class);
     }
 }
