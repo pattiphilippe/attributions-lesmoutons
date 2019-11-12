ea0e7ed991c0f6011f058964a01fe0999d7ca720
Anas Touda Lachiri <43256@etu.he2b.be>
2019-11-12 17:37:36 +0100

    Remove csc import attributions

--- resources/views/attributions/index.blade.php
+++ resources/views/attributions/index.blade.php
@@ -40,20 +40,6 @@
 </table>
 @endif
 
-<!-- Form -->
-<form method='post' action='{{ route('upload_professor') }}' enctype='multipart/form-data'>
-    @csrf
-    <div class="input-group">
-        <div class="input-group-prepend">
-            <input class="btn btn-info" type='submit' name='submit' value='Import' id='import-csv-button'> </div>
-        <div class="custom-file">
-            <input type="file" name="file" class="custom-file-input" id="import-csv">
-            <label class="custom-file-label" for="import-csv">Choose csv file</label>
-        </div>
-    </div>
-</form>
-
-
 <div class="buttonBloc">
     <button type="button" id="accueilBtn" name="accueilBtn" onclick="window.location='{{ route('index') }}' "> > vers
         accueil </button>
