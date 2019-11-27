b31596c8415591195de54eee76aac06a14e001e6
Anas Touda Lachiri <43256@etu.he2b.be>
2019-11-27 20:53:26 +0100

    Finish test on filters of courses

--- tests/Browser/CoursesListTest.php
+++ tests/Browser/CoursesListTest.php
@@ -4,6 +4,8 @@ namespace Tests\Browser;
 
 use App\Course;
 use App\Attribution;
+use App\Groupe;
+use App\Professeur;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -11,154 +13,114 @@ use Tests\DuskTestCase;
 class CoursesListTest extends DuskTestCase
 {
     use DatabaseMigrations;
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testWithCourses()
-    {
-        $user = factory(\App\User::class)->create();
-        factory(Course::class, 100)->create();
-        $this->browse(function (Browser $browser) use ($user) {
-            $browser->loginAs($user)
-                ->visit('/courses')
-                ->assertTitle('Liste des Cours')
-                ->assertSee('Acronyme')
-                ->assertSee('Titre')
-                ->assertSee('Ects')
-                ->assertSee("Nombre d'heures pour le 1er bimestre")
-                ->assertSee("Nombre d'heures pour le 2e bimestre");
-        });
-    }
 
     /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testWithoutCourses()
-    {
-        $user = factory(\App\User::class)->create();
-        $this->browse(function (Browser $browser) use ($user) {
-            $browser->loginAs($user)
-                ->visit('/courses')
-                ->assertTitle('Liste des Cours')
-                ->assertSee('La liste est un peu vide!');
-        });
-    }
-
-    /**
-     * A Dusk test example.
+     * Test with a course attributed to 1 group and there exist only 1 group so the course is considered to be attributed
      *
      * @return void
      */
     public function testWithCoursesAttributed()
     {
         $user = factory(\App\User::class)->create();
-        $attribution = factory(Attribution::class, 10)->create([
-            'course_id' => 'PRJG5',
+        $course = factory(Course::class)->create([
+            'id' => 'DONG5',
         ]);
-        $this->browse(function (Browser $browser) use ($user, $attribution) {
+        $groupe = factory(Groupe::class)->create([
+            'nom' => 'A2',
+        ]);
+        factory(Attribution::class)->create([
+            'course_id' => $course->id,
+            'group_id' => $groupe->nom,
+        ]);
+        $this->browse(function (Browser $browser) use ($user, $course) {
             $browser->loginAs($user)
                 ->visit('/courses')
                 ->assertTitle('Liste des Cours')
+                ->select('filter', 'coursesNonAttributed')
+                ->assertDontSee($course->id)
                 ->select('filter', 'coursesAttributed')
-                ->assertSee($attribution->course_id);
+                ->assertSee($course->id);
         });
     }
 
     /**
-     * A Dusk test example.
+     * Test with a course attributed to 1 group and there exist  2 group so the course is considered to be not attributed
      *
      * @return void
      */
     public function testWithCoursesNonAttributed()
     {
         $user = factory(\App\User::class)->create();
-        $attribution = factory(Attribution::class)->create();
-        $this->browse(function (Browser $browser) use ($user, $attribution) {
-            $browser->loginAs($user)
-                ->visit('/courses')
-                ->assertTitle('Liste des Cours')
-                ->select('filter', 'coursesNonAttributed')
-                ->assertSee($attribution->id);
-        });
-    }
-
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testWithCoursesNonAttributedDontSee()
-    {
-        $user = factory(\App\User::class)->create();
-        $course = factory(Course::class)->create();
+        $course = factory(Course::class)->create([
+            'id' => 'DONG5',
+        ]);
+        $groupe = factory(Groupe::class)->create([
+            'nom' => 'A2',
+        ]);
+        factory(Groupe::class)->create([
+            'nom' => 'A3',
+        ]);
+        factory(Attribution::class)->create([
+            'course_id' => $course->id,
+            'group_id' => $groupe->nom,
+        ]);
         $this->browse(function (Browser $browser) use ($user, $course) {
             $browser->loginAs($user)
                 ->visit('/courses')
                 ->assertTitle('Liste des Cours')
+                ->select('filter', 'coursesNonAttributed')
+                ->assertSee($course->id)
                 ->select('filter', 'coursesAttributed')
                 ->assertDontSee($course->id);
         });
     }
 
     /**
-     * A Dusk test example.
+     * Test with a course attributed and course not attributed tested with all filters
      *
      * @return void
      */
-    public function testWithCoursesAttributedDontSee()
+    public function testWithAllCoursesAttributedAndNot()
     {
         $user = factory(\App\User::class)->create();
-        $attribution = factory(Attribution::class)->create();
-        $this->browse(function (Browser $browser) use ($user, $attribution) {
-            $browser->loginAs($user)
-                ->visit('/courses')
-                ->assertTitle('Liste des Cours')
-                ->select('filter', 'coursesNonAttributed')
-                ->assertDontSee($attribution->course_id);
-        });
-    }
-
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testWithCoursesSeeWithoutFilter()
-    {
-        $user = factory(\App\User::class)->create();
-        $attribution = factory(Attribution::class)->create();
-        $course = factory(Course::class)->create();
-        $this->browse(function (Browser $browser) use ($user, $attribution, $course) {
-            $browser->loginAs($user)
-                ->visit('/courses')
-                ->assertTitle('Liste des Cours')
-                ->select('filter', 'courses')
-                ->assertSee($attribution->course_id)
-                ->assertSee($course->id);
-        });
-    }
-
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testWithAllCourseAttributedWithFilterNonAttributed()
-    {
-        $user = factory(\App\User::class)->create();
-        $attribution = factory(Attribution::class, 10)->create([
-            'course_id' => 'DONG5',
+        $course = factory(Course::class)->create([
+            'id' => 'DONG5',
         ]);
-        $this->browse(function (Browser $browser) use ($user, $attribution) {
+        $course2 = factory(Course::class)->create([
+            'id' => 'MOBG5',
+        ]);
+        $groupe = factory(Groupe::class)->create([
+            'nom' => 'A2',
+        ]);
+        $groupe2 = factory(Groupe::class)->create([
+            'nom' => 'A3',
+        ]);
+        factory(Attribution::class)->create([
+            'course_id' => $course->id,
+            'group_id' => $groupe->nom,
+        ]);
+        factory(Attribution::class)->create([
+            'course_id' => $course->id,
+            'group_id' => $groupe2->nom,
+        ]);
+        factory(Attribution::class)->create([
+            'course_id' => $course2->id,
+            'group_id' => $groupe->nom,
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user, $course, $course2) {
             $browser->loginAs($user)
                 ->visit('/courses')
                 ->assertTitle('Liste des Cours')
                 ->select('filter', 'coursesNonAttributed')
-                ->assertDontSee($attribution->course_id);
+                ->assertSee($course2->id)
+                ->assertDontSee($course->id)
+                ->select('filter', 'coursesAttributed')
+                ->assertSee($course->id)
+                ->assertDontSee($course2->id)
+                ->select('filter', 'courses')
+                ->assertSee($course2->id)
+                ->assertSee($course->id);
         });
     }
 }
