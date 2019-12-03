c8086636beb1da92f88b277fa5026d37a50ea237
Piekarski Maciej <44343@etu.he2b.be>
2019-12-03 15:05:20 +0100

    Ajout de tests supplementaires

--- tests/Browser/AttributionGroupingTest.php
+++ tests/Browser/AttributionGroupingTest.php
@@ -38,7 +38,7 @@ class AttributionGroupingTest extends DuskTestCase
         ]);
     }
 
-    public function testExample()
+    public function testGroupingByGroupWorkTest()
     {
         $this->initTables();
         $user = factory(User::class)->create();
@@ -59,6 +59,42 @@ class AttributionGroupingTest extends DuskTestCase
                 ->waitForText('E11')
                 ->assertPresent('#table-E12')
                 ->assertPresent('#table-E11');
+        });
+
+    }
+
+    public function testGroupingByGroupWorkTitlesTest()
+    {
+        $this->initTables();
+        $user = factory(User::class)->create();
+
+        $attributions = [
+            $this->createAttribution("WEBG5", "JLC", "E12"),
+            $this->createAttribution("DEV4", "NVS", "E11"),
+            $this->createAttribution("SYSG5", "MBA", "E12"),
+            $this->createAttribution("WEBG5", "JLC", "E11"),
+            $this->createAttribution("PRJG5", "SRV", "E12"),
+        ];
+
+        $this->browse(function (Browser $browser) use ($user, $attributions) {
+            $browser->loginAs($user)
+                ->visit('/attributions')
+                ->select('groupby', 'course')
+                ->select('groupby', 'group')
+                ->waitForText('E11')
+                ->assertSeeIn('h3:nth-of-type(1)', 'E12')
+                ->assertSeeIn('h3:nth-of-type(2)', 'E11');
+        });
+    }
+
+    public function testGroupingByGroupEmptyAttributions()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->visit('/attributions')
+                ->assertSee('La liste est vide');
         });
 
     }
